af0cb5fe58a649fd4db9e2bcc19bac5db4d66bec
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-01-26 16:31:16 -0300

    Armada la estructura para implementar la logica de 2 arrays numericos y un operador. Creados los eventos para todos los botones.

--- javascript.js
+++ javascript.js
@@ -30,46 +30,89 @@ function calculo(operandoA, operador, operandoB) {
     }
 }
 
+const operando1 = [0,];
+const operando2 = [];
+const operadorObj = []; 
+
 const numeroEnPantallaBaja = document.querySelector(".parteInferior");
-const botonesOperando = document.querySelectorAll('#numeros > button');
+const numerosEnPantallaBaja = document.querySelectorAll('.parteInferior > p');
+const botonesOperando = document.querySelectorAll('#tecla');
+
 
 botonesOperando.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(button.value);
-        switch (button.value) {
-            case "0":
-                numeroEnPantallaBaja.textContent = 0;
-                break;
-            case "1":
-                numeroEnPantallaBaja.textContent = 1;
-                break;
-            case "2":
-                numeroEnPantallaBaja.textContent = 2;
-                break;
-            case "3":
-                numeroEnPantallaBaja.textContent = 3;
-                break;
-            case "4":
-                numeroEnPantallaBaja.textContent = 4;
-                break;
-            case "5":
-                numeroEnPantallaBaja.textContent = 5;
-                break;
-            case "6":
-                numeroEnPantallaBaja.textContent = 6;
-                break;            
-            case "7":
-                numeroEnPantallaBaja.textContent = 7;
-                break;    
-            case "8":
-                numeroEnPantallaBaja.textContent = 8;
-                break;
-            case "9":
-                numeroEnPantallaBaja.textContent = 9;
-                break;    
-            default: 0
-        }
+        if (button.className === "operando") {
+            switch (button.value) {
+                case "0":
+                    operando1.push(0);
+                    break;
+                case "1":
+                    operando1.push(1);
+                    break;
+                case "2":
+                    operando1.push(2);
+                    break;
+                case "3":
+                    operando1.push(3);
+                    break;
+                case "4":
+                    operando1.push(4);
+                    break;
+                case "5":
+                    operando1.push(5);
+                    break;
+                case "6":
+                    operando1.push(6);
+                    break;            
+                case "7":
+                    operando1.push(7);
+                    break;    
+                case "8":
+                    operando1.push(8);
+                    break;
+                case "9":
+                    operando1.push(9); 
+                    break;              
+            }    
+        } else if (button.className === "operador") {
+            switch (button.value) {
+                case "+":
+                    operadorObj.splice(0, 1, "+");
+                    break;    
+                case "-":
+                    operadorObj.splice(0, 1, "-");
+                    break;    
+                case "*":
+                    operadorObj.splice(0, 1, "*");
+                    break;
+                case "/":
+                    operadorObj.splice(0, 1, "/");
+                    break;                                     
+            } 
+        } else if (button.className === "simbolo") {
+            switch (button.value) {  
+                case "=":
+                    numeroEnPantallaBaja.textContent = "=";
+                    console.log(operando1 + operadorObj);
+                    break;
+                case "C":
+                    numeroEnPantallaBaja.textContent = "C";
+                    break;                    
+            }
+        }         
+        
     });
 });
+
+//function mostrarEnPantalla(operadores, operandos) {
+    
+//}
+
+console.log(operando1)
+//const pulsoOperadorOSimbolo = pantallaArriba.some(function(pulsaciones) {
+//    if(currentYear - person.year >= 19) {
+//        return true;
+//    }
+//});
 
 //calculo(2,"-",3)
